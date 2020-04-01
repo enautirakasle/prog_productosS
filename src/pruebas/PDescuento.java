f5768e8ac87c0384a586103b19e2d870eb4f76bf
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-04-01 12:52:17 +0200

    Modelo txikiak eta probak egin

---
+++ src/pruebas/PDescuento.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+import modelo.dao.ModeloDescuento;
+
+public class PDescuento {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ModeloDescuento me = new ModeloDescuento();
+		System.out.println(me.getAll());
+
+	}
+
+}
