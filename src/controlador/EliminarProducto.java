d2686066196c01af62d2747af932150a22c5a9d1
Eñaut Agirre <enaut.irakasle@gmail.com>
2020-04-21 10:53:49 +0200

    Eliminar producto funtzionalitatea

---
+++ src/controlador/EliminarProducto.java
@@ -0,0 +1,55 @@
+package controlador;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloProducto;
+
+/**
+ * Servlet implementation class EliminarProducto
+ */
+@WebServlet("/EliminarProducto")
+public class EliminarProducto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EliminarProducto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		
+		try {
+			ModeloProducto mProducto = new ModeloProducto();
+			mProducto.delete(id);
+			mProducto.getConexion().close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("VerProductos");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
